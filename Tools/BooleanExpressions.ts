76f4fe3d5972775425b1aee4f1198df83542b22a
Florian Crafter <55284712+FlorianStrobl@users.noreply.github.com>
2021-08-27 17:33:32 +0200

    merged binary numbers and number parser to one file

---
+++ Tools/BooleanExpressions.ts
@@ -0,0 +1,84 @@
+export class BooleanExp {
+  public bool: boolean;
+
+  // add <,>,>=,<=,== as feature TODO
+
+  // () ! && || true false
+  constructor(booleanExpression: string) {
+    this.bool = this.getBool(booleanExpression);
+
+    return this;
+  }
+
+  private getBool(boolExp: string): boolean {
+    boolExp = boolExp.replace(/ /g, (s) => ''); // remove unwanted spaces
+    // check for invalid characters
+    if (!boolExp.match(/^(true|false|\(|\)|!|&&|\|\|)+$/))
+      throw new Error('Invalid symbol used.');
+
+    boolExp = removeSimpleNots(boolExp);
+
+    let res: string[][] = [];
+    for (let db of getDeepestBrakets(boolExp)) // get all the deepest ()
+      res.push([db, evaluateBoolWithoutBrakets(db.slice(1, -1))]); // evaluate them
+    for (let r of res) boolExp = boolExp.replace(r[0], () => r[1]); // save them
+
+    boolExp = removeSimpleNots(boolExp);
+
+    if (!boolExp.includes('(')) boolExp = evaluateBoolWithoutBrakets(boolExp);
+
+    // recursion if still () else finish
+    if (boolExp.includes('(')) return this.getBool(boolExp);
+    else {
+      if (boolExp === 'true') return true;
+      else if (boolExp === 'false') return false;
+      else throw new Error('Error happend during conversion: ' + boolExp);
+    }
+
+    function removeSimpleNots(str: string): string {
+      // replace !true => false, !false => true (without () check)
+      return str.replace(/!(true|false)/g, (b) =>
+        b.includes('true') ? 'false' : 'true'
+      );
+    }
+
+    function evaluateBoolWithoutBrakets(str: string): string {
+      // symbols: ! true false && ||
+
+      str = removeSimpleNots(str);
+
+      if (str.includes('&&') && str.includes('||')) {
+        // get all the &&, evaluate them and then get do the simple ||
+        let ands: string[] = str
+          .split('||')
+          .map((s) => evaluateBoolWithoutBrakets(s));
+        return ands.includes('true') ? 'true' : 'false';
+      } else if (str.includes('&&') && !str.includes('||'))
+        return removeSimpleNots(str).includes('false') ? 'false' : 'true';
+      else if (!str.includes('&&') && str.includes('||'))
+        return removeSimpleNots(str).includes('true') ? 'true' : 'false';
+      else return removeSimpleNots(str); // finish
+    }
+  }
+}
+
+function getDeepestBrakets(str: string): string[] {
+  let write: boolean = false;
+  let curVal: string = '';
+  let vals: string[] = [];
+
+  for (let b of str) {
+    if (b === '(') {
+      write = true;
+      curVal = '';
+    } else if (b === ')' && write) {
+      write = false;
+      vals.push(curVal + ')');
+      curVal = '';
+    }
+
+    if (write) curVal += b;
+  }
+
+  return vals;
+}
--- Tools/Math.ts
+++ Tools/Math.ts
@@ -14,6 +14,27 @@ export namespace Math {
     export function Factorial() {}
   }
 
+  export namespace BitManipulation {
+    export function And() {}
+
+    export function Or() {}
+    export function Xor() {}
+    export function Not() {}
+
+    // Circular shift, Logical shift, Arithmetic shift
+    export function ShiftLeft() {}
+    export function ShiftRight() {}
+
+    /**
+     * clear from specified bit position up (leave lower part of word)
+clear from specified bit position down (leave upper part of word)
+mask from low bit down (clear lower word)
+mask from high bit up (clear lower word)
+bitfield extract
+bitfield insert
+     */
+  }
+
   export namespace ArbitraryPrecisionArithmetic {
     export function Add() {}
     export function Subtract() {}
